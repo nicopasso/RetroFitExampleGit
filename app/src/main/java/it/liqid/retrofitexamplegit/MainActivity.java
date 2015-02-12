4a7ee52d2d3e422f12036ac0aa1eb8aac2d0ed88
nicopasso <nicopasso@gmail.com>
2015-02-12 17:19:26 +0100

    Initial commit

---
+++ app/src/main/java/it/liqid/retrofitexamplegit/MainActivity.java
@@ -0,0 +1,77 @@
+package it.liqid.retrofitexamplegit;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.List;
+import java.util.Observable;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+import retrofit.http.GET;
+import retrofit.http.Path;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint("https://api.github.com")
+                .build();
+
+        GithubService service = restAdapter.create(GithubService.class);
+
+        service.listRepos("nicopasso", new Callback<List<Repo>>() {
+            @Override
+            public void success(List<Repo> repos, Response response) {
+                 for (Repo repo : repos) {
+                     Log.e("REPO NAME", repo.name);
+                 }
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+
+            }
+        });
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public interface GithubService {
+        @GET("/users/{user}/repos")
+        void listRepos(@Path("user") String user, Callback<List<Repo>> callback);
+    }
+}
---
+++ app/src/main/java/it/liqid/retrofitexamplegit/Repo.java
@@ -0,0 +1,19 @@
+package it.liqid.retrofitexamplegit;
+
+/**
+ * Created by niccolo on 12/02/15.
+ */
+public class Repo {
+
+    public Integer id;
+    public String name;
+    public String html_url;
+    public boolean fork;
+    public String url;
+    public String forks_url;
+    public String keys_url;
+    public String created_at;
+    public String updated_at;
+    public String pushed_at;
+
+}
